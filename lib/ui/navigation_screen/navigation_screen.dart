28b677a4cdf4ae6bc642c5a135a6d9f1e5466878
Hung Nguyen <hung.nguyen@signanthealth.com>
2021-10-06 22:19:31 +0300

    Navigation_screen with tabs

--- lib/ui/navigation_screen/navigation_screen.dart
+++ lib/ui/navigation_screen/navigation_screen.dart
@@ -1,15 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:tinder_clone/common/palette.dart';
+import 'package:tinder_clone/ui/screens.dart';
 
-class NavigationScreen extends StatelessWidget {
+class NavigationScreen extends StatefulWidget {
   const NavigationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<NavigationScreen> createState() => _NavigationScreenState();
+}
+
+class _NavigationScreenState extends State<NavigationScreen> {
+  late int _selectedIndex;
+  final _tabIcons = [Icons.local_fire_department_rounded, Icons.star];
+  final _tabIconColors = [pink, yellow];
+
+  Widget _buildIcon(int index) {
+    return IconButton(
+      onPressed: () => _onTabPressed(index),
+      color: _selectedIndex == index ? _tabIconColors[index] : grey,
+      icon: Icon(_tabIcons[index]),
+    );
+  }
+
+  void _onTabPressed(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Widget _getBody() {
+    return IndexedStack(
+      index: _selectedIndex,
+      children: [HomeScreen(), LikeScreen()],
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedIndex = 0;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: white,
+      appBar: AppBar(
+        backgroundColor: white,
+        elevation: 0.0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: _tabIcons
+              .asMap()
+              .entries
+              .map((mapEntry) => _buildIcon(mapEntry.key))
+              .toList(),
+        ),
       ),
+      body: _getBody(),
     );
   }
 }
